59cf37466a69aee3b3f8aeb12a23d35e4b7c42f0
nicolas-lamb <nikelamb@gmail.com>
2024-06-17 15:38:34 -0300

    abrir postagem

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-angular.module('blog', []);
-angular.module('blog').controller('Rest', function ($scope, $http){
-  $http.get('https://api-fake-blog.onrender.com/postagens').
-      success(function(data) {
-          $scope.publicacoes = data;
-      }
-  );
-});
+angular.module('blog', [])
+.controller('Rest', function($scope, $http, $window) {
+  $http.get('https://api-fake-blog.onrender.com/postagens')
+    .success(function(data) {
+      $scope.publicacoes = data;
+    });
 
-angular.module('post', []);
-angular.module('post').controller('Rest', function ($scope, $http){
-    
-  $http.get('https://api-fake-blog.onrender.com/postagem/0').
-      success(function(data) {
-          $scope.publicacao = data;a
-      }
-  );
+  $scope.viewPost = function(postId) {
+    $window.location.href = 'post.html?id=' + postId;
+  };
+})
+.controller('Postagem', function($scope, $http, $window) {
+  const queryParams = new URLSearchParams($window.location.search);
+  const postId = queryParams.get('id');
+
+  if (postId) {
+
+    $http.get(`https://api-fake-blog.onrender.com/postagem/`+postId)
+      .success(function(data) {
+        $scope.publicacao = data;
+      });
+      
+  }
 });
